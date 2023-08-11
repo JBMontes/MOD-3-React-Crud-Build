2061d4288d3d3afa65bc16e353d4e9291414ee16
Jeremy Montes <jeremymontes@pursuit.org>
2023-08-11 18:23:31 -0400

    React app started, boiler-plate code removed, install react router v6

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Router>
+
+      <h1>Screen Views, Hey</h1>
+
+      </Router>
+    </>
+  )
+}
+
+export default App
