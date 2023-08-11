bff8c17ae63a8c5a96806057aca315808382f167
Jeremy Montes <jeremymontes@pursuit.org>
2023-08-11 18:57:14 -0400

    add env and one test variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
-
 import './App.css'
+
 
 function App() {
  
-
+console.log("Our Environment Variable: ", import.meta.env.VITE_10_1_VAR )
   return (
     <>
       <Router>
 
       <h1>Screen Views, Hey</h1>
 
+    <h2>  TEST-{import.meta.env.VITE_10_1_VAR}</h2>
       </Router>
     </>
   )
